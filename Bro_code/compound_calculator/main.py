8119e7bbd196856146188da5634dac03d34c5335
1Gift3 <joshuaramaoka@gmail.com>
2025-03-10 17:41:00 +0200

    Documentation

--- Bro_code/compound_calculator/main.py
+++ Bro_code/compound_calculator/main.py
@@ -29,3 +29,9 @@ print(f"Balance after {time} years/s: R{total:.2f}")
 
 # This has two way of working it with While True and another first way
 
+# While statements are used for repeated execution as long as expression it true
+# - If its true it execute if flase the suit of the else clause if present is executed and the loop terminates
+
+# Break statement is executed after executing else clause
+
+# What it does allow me to do here is to if its supports the clause thats is the inputed by user is less than zero print statement else: break to the following loop state
