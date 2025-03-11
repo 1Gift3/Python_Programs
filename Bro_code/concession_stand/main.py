99d86b20519000af241dd2effca1075d2d142a1c
1Gift3 <joshuaramaoka@gmail.com>
2025-03-11 18:05:38 +0200

    Revised docum

--- Bro_code/concession_stand/main.py
+++ Bro_code/concession_stand/main.py
@@ -32,3 +32,11 @@ print(f"Total is: R{total:.2f}")
 # Seems like popcorn doesnt print out 
 
 # Somehow the next day it works.
+#   WHAT
+# Question is Why would it be useful to assist with Small tasks to Small - vendors,small businesses, entreprenuers.
+# It simplifies operations, boosts fundraising efforts, and provides valuable data that can inform better decisions.
+#  How
+# It can be useful too if a graph user interface comes in with payments ethods or a more detailed inventory management like adding Quantity.
+# it would be useful to enhance efficiency, profitabilyty the overall experience for both vendors and customers. It simplifies operations, boosts fundraising efforts, and provides valuable data that can inform better decisions.
+#    WHAT
+#  It helps a Business to keep track of purchases, processing orders and callculate the total
