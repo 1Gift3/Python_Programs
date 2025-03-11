826ddc15e88f58641ddccfff039f1e311a3be172
1Gift3 <joshuaramaoka@gmail.com>
2025-03-11 18:08:13 +0200

    Interesting

--- Bro_code/email_slicer/main.py
+++ Bro_code/email_slicer/main.py
@@ -11,3 +11,18 @@ print(f"Your username is {username} and domain is {domain}")
 
 # Indexing helps me to find the @ symbol positions while the sclicing extracts parts of it
 # the []:index] gets everything before @ then the [+ 1:]after the @ 
+
+# Its because it lets use easily access specific elements in a list []
+#  # list_name [start:end: step]
+# - This step is useful because it specifys the interval between elemnts dafault to 1 if ommited
+# List slice allows outof bound indexing without errors - we simply specify indicies beyond length then it will return the available
+
+
+# It can be used by [::]
+#  - in simple is get every [::2] second element from the list
+# - the []:index] gets everything before @ then the [+ 1:]after the @ 
+# Another way that it can be used is using negative indexing accessing elments from the end of list -1, -2.
+# We can also reverse the entire list by using [::-1] - indicates that Python should traverse the list in reverse order, starting from the end. The slice a[::-1] starts from the end of the list and moves to the beginning which result in reversing list
+
+
+# It lets us easily access specific elements in a List[]
