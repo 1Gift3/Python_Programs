3501d186a29f340070ea46e1e56d2e36d26e1078
1Gift3 <joshuaramaoka@gmail.com>
2025-03-11 18:07:24 +0200

    This was interesting

--- Bro_code/credit_card_validator/main.py
+++ Bro_code/credit_card_validator/main.py
@@ -1,12 +1,35 @@
 # Pythons credit card
 
 # Removes any '-' or ' '
-#add all digits in the places from the right to left 
+# add all digits in the places from the right to left 
 # double every second digit from right to left
     # IF RESULT IS A TWO DIGIT NUMBER
-    #ADD THE TWO digit number together to the single digit
+    # ADD THE TWO digit number together to the single digit
 # sum the totals of steps 2 & 3
 # if sum is divisible by 10, the credit cards # is valid
+
+#  First of all why im using Luhn Algo is because its commonly used to vslidate credit card numbas
+# Why it works is becaause this Modulus 10 it was crerated or being used to identify credit cards IMEI no. and canandian social insurance numbers
+# - its simple method to distinguish valid numbers from mistyped or incorrect, designed to protect against accidental errors not malicious attacks
+
+# How it is used is that first step
+# - it starts from your numba from the rightmost with a double on the value of every second digit
+# - if the doubling result in two digits, you add those digits toget a single numba
+# - if you still get double youll have to add again
+# - once done take the sum of all digits
+# - if the total ends in zero the card numba is valid
+# Now my code here still does the same but now removes - or ' ' the proceeds with the steps of Mod 10
+#  We can incorporate other card types by prefix and length
+# Visa: Starts with 4, 13 or 16 digits.
+# MasterCard: Starts with 51–55 or 2221–2720, 16 digits.
+# American Express: Starts with 34 or 37, 15 digits.
+# Discover: Starts with 6011, 622126–622925, 644–649, or 65, 16 digits.
+
+# This reads a card numba to validate whether is works or not
+# - cleaning the card numba
+# - You can add reverse for easy right to left
+# - doubles every second digit
+# - calculates the toal sum then checks for validity.
 
 sum_odd_digits = 0 
 sum_even_digits = 0 
