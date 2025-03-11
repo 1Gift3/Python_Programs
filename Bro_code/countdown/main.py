88df466541fb0b6795397b6b3a6c4f124ba74657
1Gift3 <joshuaramaoka@gmail.com>
2025-03-11 18:06:26 +0200

    Updated Readme

--- Bro_code/countdown/main.py
+++ Bro_code/countdown/main.py
@@ -9,3 +9,7 @@ for x in range(my_time, 0, -1):
     time.sleep(1)
 
 print("Times up")
+
+#  Count down would be useful in so many ways from ecommerce to dealines on projects in radio public awareness etc it acts as a movitation to act creating a sense of urgency
+
+#  It can help you breakdowns tasks into manageable chuncks from time blocking small tasks e.g relax for 15 min, prioritizingand completing tasks 10 min countdown, increase focus with tracking progress on multiple small tasks, supporting small business operations admin tasks or customers service calls  
