0a7777f21c562b06ec720dd525cf2a85127525d2
1Gift3 <joshuaramaoka@gmail.com>
2025-03-25 13:11:27 +0200

    My note refreshed on temp, cart and rock

--- Bro_code/rock_papper/main.py
+++ Bro_code/rock_papper/main.py
@@ -32,3 +32,33 @@ while running:
         running = False
 
 print("Thanks for playing!")        
+
+
+#WHY
+# It is useful for various reasons like decision making tool.
+# Its a great way also for beginners to practice programming coding , logic and conditional statements
+# Gives fundamentals of game theory concepts like probability and startegy and also it is useful for teaching probability and statistic.
+# Gives also insights that people often try to predict patterns making it interesting for psychology used in experiments on decision making and randomness.
+# It helps develop quick thinking pattern recognition and strategic decision making.
+
+
+#HOW
+# It can be used to build an AI that learns patterns and tries to predicts the users next move
+# And also analyze players strategies to see if there's a bias in their choices.
+
+#WHAT
+# The random module is imported to use random.choice() function which randomly selects an option for the pc
+# A tuple options is created with the 3 possible choices
+# The running variable controls the main game loop and it continues until the player decideds to stop
+# Player is initialized to none to start and the computer randomly selects an option using random.choice((options)
+# The program keeps asking the player for a valid choice until they enter the options
+# Display choices of both the player and th e pc are displayed using formatted strings
+# If both choices are the same its a tie, the players wins if their choice beats the pc choice arccording to the games rules otherwise the player loses.
+# Players again option - the player is prompted to play again, if they enter anything others than "y" the game ends.
+# When the player decides to quit, a farewell message is displayed.
+
+# Improvements 
+# Handling invalid inputs like spellling mistakes more gracefully
+# keep track of players score across multple rounds 
+# add a best of 3 or best of 5 features
+
--- Bro_code/shopping_cart/main.py
+++ Bro_code/shopping_cart/main.py
@@ -21,3 +21,30 @@ for price in prices:
 
 print()
 print(f"Your total is: ${total}")        
+
+# WHY
+# It helps learn programming basics like data structures demonstrating the use of lists (food and prices) to store multiple values
+# Control flow and loops using a while loop and if-else for input
+# String formatting using f-strings to format and display info
+# and type conversions showing th e usse of float() for converting input to numeric data.
+# We can also use it for real world app - point of sales systems : like the logic mirror basic POS systems where items and price are entered, displayed and totaled.
+# Budget tracking and inventory management.
+
+# HOW
+# Can use it for grocery planning.
+# Can expand it to build a GUI: using a library like tkinter or pyqt to make it graphical desktop app
+# Database intergration to store items and prices 
+# Web App turning it into a web app using django and react
+# And also mobile app using kivy(python) or react native.
+
+# WHAT
+# The user can enter food items along with their prices view their cart and sees the total cost before exiting.
+# Takes the user input
+# stores data item names are saved in the foods list and prices list
+# and displays the cart, by listing all the items you addded and calculates and shows the total cost of all the items.
+
+# IMprovements
+# Error handling inputs for prices
+# displays enhancements, displaying item names with their respective prices
+# And Advaced feautures, allowing users to remove items from the cart, add ing tax calculations and tracking quantities of each item.
+
