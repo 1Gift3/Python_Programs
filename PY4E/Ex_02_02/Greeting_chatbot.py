2bc40b27df6818f1844d431dedc7aae71bfc2296
1Gift3 <joshuaramaoka@gmail.com>
2025-03-28 10:04:28 +0200

    update all

---
+++ PY4E/Ex_02_02/Greeting_chatbot.py
@@ -0,0 +1,30 @@
+name = input("enter your name:")
+print(f"Hello, {Nice to meet you.}")
+
+# Asking a follow up Q
+mood = input ("What would you say you feel like right now?")
+
+# Respond based mood
+if mood.lower() in ["good", "great", "happy", "awesome"]:
+    print("Thats great that you feeling this way! Keep up being positive! 😊")
+elif mood.lower() in ["sad", "tired", "not good", "bad"]:
+    print("It happens sometimes, Its not the end...go through it can get what need it from it! 🌻")
+else:
+    print("Thats interesting! Hope you have a fantastic day ahead of you and be in every moment!")
+
+# Asking another question
+hobby = input("WHats yor favourite hobby?")
+print(f"That sounds interesting, {name}! {hobby.capitalized()} is a great way to spend time.")
+
+print("Thanks for chatting with me! Have a great one! 🚀")
+
+
+
+
+
+
+
+
+
+
+# Got reminded on how to use emojis again Windows, + and period .
