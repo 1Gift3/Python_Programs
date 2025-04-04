7f9f61ea3289e706d3b7d21e3b0672b9b2fb3aed
1Gift3 <joshuaramaoka.com>
2025-04-04 11:39:24 +0200

    formatter notes

--- PY4E/Ex_02_03/budgeting_and_financial_planning/main.py
+++ PY4E/Ex_02_03/budgeting_and_financial_planning/main.py
@@ -2,7 +2,7 @@
 hrs_per_week = float(input("Enter hours worked per week: "))
 hourly_rate = float(input("Enter your hourly rate: $"))
 weeks_per_month = 4 # Assuming 4 weeks in a months 
-tax_rate = 0.10 # Flat 10% tax
+tax_rate = 0.14 # Flat 14% tax
 saving_goal_percentage = float(input("Enter the percentage of income you want to save (e.g, 20 for 20%): ")) / 100
 
 # Calculate gross pay (before tax)
@@ -19,8 +19,14 @@ net_monthly_pay = gross_monthly_pay - tax_deductions
 monthly_savings = net_monthly_pay * saving_goal_percentage
 
 # Display results
-print(f"nYour gross monthly pay (before tax): ${gross_monthly_pay:.2f}")
-print(f"Your tax deductions (10%): ${tax_deductions:.2f}")
+print(f"\nYour gross monthly pay (before tax): ${gross_monthly_pay:.2f}")
+print(f"Your tax deductions (14%): ${tax_deductions:.2f}")
 print(f"Your net monthly pay (after tax): ${net_monthly_pay:.2f}")
 print(f"Your monthly savings goal({saving_goal_percentage*100}%): ${monthly_savings:.2f}")
 
+
+# %f - is a formatter specifically used for formatting float values, to specify the number of the decimal numbers to be returned when a floating point is rounded up.
+# %.2f means to round up by two decimal places.
+
+# HOW
+# We can use another formatter %d in python - which returns the whole number in a floating point.
