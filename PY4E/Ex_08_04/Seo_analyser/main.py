db6a0e9d1448e3cbd83bbe0ab88b1f5776db6e7b
1Gift3 <joshuaramaoka@gmail.com>
2025-04-25 18:01:05 +0200

    new script for Seoo analyzer

---
+++ PY4E/Ex_08_04/Seo_analyser/main.py
@@ -0,0 +1,37 @@
+# WHAT
+# Building an seo keyword frequency analyzer that normalizing words, removing stop words and counting word frequency
+
+
+import string
+from collections import Counter 
+from nltk.corpus import stopwords
+import nltk
+
+# Downloading stopwords the first time you run this
+nltk.download('stopwords')
+
+# opening and reading the file content
+with open("romeo.txt", "r") as file:
+    text = file.read().lower()
+
+# splitting text into individual words
+raw_words = text.split()    
+
+# removing punctuation from each word
+cleaned_words = [word.strip(string.punctuation) for word in raw_words]
+
+# loading english stopwords(e.g "the", "and" etc)
+stop_words = set(stopwords.words('english'))
+
+# filtering out stopwords and empty strings
+filtered_words = [word for word in cleaned_words if word and word not in stop_words]
+
+# counting the frequency of each word
+word_counts = Counter(filtered_words)
+
+# Printing the top N most common words
+print("Top keywords by frequency:\n")
+for word, count in word_counts.most_common(20): # changing 20 to anu number you want
+    print(f"{word}: {count}")
+
+
