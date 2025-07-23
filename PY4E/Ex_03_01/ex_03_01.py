f5e3862c1ced0d9d80d4b0dc3d47dd210399f09f
1Gift3 <joshuaramaoka@gmail.com>
2025-07-23 17:51:06 +0200

    Readme

--- PY4E/Ex_03_01/ex_03_01.py
+++ PY4E/Ex_03_01/ex_03_01.py
@@ -19,35 +19,25 @@ if ih > 40 :
 else:
   #Print ("Reg_pay")
   Xy = ih * fr
+  print(Xy)
 
 
 
-# WHAT
-# A simple pay cal helping compute weekly earnings of an employee
-# Based on number of hours worked and houly rate
-# It takes the user input for hours and rate
-# Converts input into appropriate data types
-# Checks if hours exceeds 40 - which is the standard time for work week
-# if yes meaning overritme
-# - It then cal's regular pay
-# - Calculates overrtime pay 
-# - and adds both to get total pay
-# if not then it just adds regular pay
-# - Multiplies hours by rate
-# LAstly it prints total pay
 
-# WHY
-# Can be used for a basic payroll cal
-# Also is a good excercise for understanding input, if-else, castin and basic arithmetic
 
-# HOW
-# Can use it to track one's pay or for small business payroll tool
-# Good for learning more on data types and conversion int() and float
-# - conditional logic (if, else)
+sh = input ("enter Hours:")
+sr = input ("Enter Rate:")
+fh = int(sh)
+fr = float(sr)
+#print(fh, fr)
 
-# IMprovements
-# Multpiple employees entries
-# Saving data to a file or database
-# a web interface
-# Exporting reports(pdf,excel)
-
+if fh > 40 :
+    #print ("overtime")
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    #print(reg, otp)
+    xp = reg + otp 
+else:
+   # print ("Regular")
+    xp = fh * fr
+print ("Pay:" ,xp)
